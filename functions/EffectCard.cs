f25e1935111c1ca1132ca5eb6a34e18aba00ad83
kcastel <keviniicastel@gmail.com>
2023-09-21 13:41:20 +0200

    implémentation des effets + docs

--- functions/EffectCard.cs
+++ functions/EffectCard.cs
@@ -6,16 +6,33 @@ namespace Triangle_Scalene{
         Program p = new Program();
 
         public void Cheval_de_troie(string card1,string card2){
-
+            /*
+                La personne qui remporte cette carte perd la moitié de sa liste carte gagné et
+                les cartes perdus sont transferée vers l'autre joueur (si cdt appartient au
+                joueur qui la remporte alors l'effet n'est pas activé)
+            */
         }
         public void Croissance_Explosive(string card1,string card2){
-           
+           /*
+             gagne les cartes s'il ya qui se trouvent dans les cartes gagnaient
+           */
         }
         // public void Grande_Revolution(string card){
         //     card.Add(p.cardBonus);
         // }
-        public void Exil(string card1,string card2){}
-        public void Reinitialisation(string card1,string card2){}
+        public void Exil(string card1,string card2){
+            /*
+            carte adverse est transférée vers le graveyards cependant la carte exil fait égalité
+            */
+        }
+        public void Reinitialisation(string card1,string card2){
+            /*
+            une fois la carte activé dans la zone de confrontation les deux zones de carte gagnées
+            se réinitialisent (toutes les cartes retourne dans la main du propriétaire de la carte)
+            cela n'affecte pas la zone de confrontation
+
+            */
+        }
         public void Hero_invincible(string card1,string card2){}
         public bool Roi(string card1,string card2){
             String str = card2;
